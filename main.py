9c454e6b99d2c7b64fe50a9d54968d6c6f5b6e1c
Hashini <hashini.liyanage@gmail.com>
2023-07-31 22:25:11 +0200

    Improvement in email

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import requests
 from send_email import send_email
 
+search_term = "tesla"
 api_key = "XXXXXXX"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
+url = "https://newsapi.org/v2/everything?" \
+      f"q={search_term}&" \
       "from=2023-06-30&sortBy=publishedAt&" \
-      "apiKey=XXXXXXX"
+      "apiKey=XXXXXXX&" \
+      "language=en"
 
 request = requests.get(url)
 content = request.json()
 
-message =""
-for article in content["articles"]:
+message = "Subject: Today's News" + "\n"
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        message = message + article["title"] + "\n" + article["description"] + 2*"\n"
+        message = message + article["title"] + "\n" \
+                  + article["description"] + "\n" \
+                  + article["url"] + 2*"\n"
 
 message = message.encode("utf-8")
 
